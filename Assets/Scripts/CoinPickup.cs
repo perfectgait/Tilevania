2c4c880a35f8a6f207470e39d0b25a470712e4d0
Matt Rathbun <mrathbun80@gmail.com>
2021-03-14 23:01:41 -0600

    Coin pickup SFX

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip CoinPickupSFX;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioSource.PlayClipAtPoint(CoinPickupSFX, Camera.main.transform.position);
+
         Destroy(gameObject);
     }
 }
