43fba7698a1d94b0f6016b8f003ff49a3e3eb0f2
Matt Rathbun <mrathbun80@gmail.com>
2021-03-15 08:43:39 -0600

    Don't respawn coins on death

---
+++ Assets/Scripts/ScenePersist.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePersist : MonoBehaviour
+{
+    private int startingSceneIndex;
+
+    private void Awake()
+    {
+        int numberOfScenePersists = FindObjectsOfType<ScenePersist>().Length;
+
+        if (numberOfScenePersists > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startingSceneIndex != SceneManager.GetActiveScene().buildIndex)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
